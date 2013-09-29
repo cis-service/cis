1bc502343660039aef81775060af7f0dad5e57f7
Voxel01 <lukas@wmm.at>
2013-09-29 21:17:12 +0200

    correct comments

--- module/Cisapi/Module.php
+++ module/Cisapi/Module.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * Central Image Service API
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @link      https://github.com/cis-service/cis
  */
 
 namespace Cisapi;
