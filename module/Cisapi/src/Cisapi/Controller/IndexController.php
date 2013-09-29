3dc8a5f5bf584485ee90b4c60c40385df0aefe00
Voxel01 <lukas@wmm.at>
2013-09-29 21:18:02 +0200

    1st api infos

--- module/Cisapi/src/Cisapi/Controller/IndexController.php
+++ module/Cisapi/src/Cisapi/Controller/IndexController.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * Central Image Service API
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @link      https://github.com/cis-service/cis
  */
 
 namespace Cisapi\Controller;
@@ -12,16 +10,35 @@ namespace Cisapi\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Zend\View\Model\JsonModel;
+use Ciscore\Model\Image;
+use Ciscore\Model\ImageTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
 
 class IndexController extends AbstractActionController
 {
+	protected $albumTable;
     public function getMetaAction()
     {
-		$result = new JsonModel(array(
-			'some_parameter' => 'some value',
-            'id'=>$this->params()->fromRoute('id',false),
-        ));
-        return $result;
+		$id = $this->getImageFromRoute();
+		$result = array();
+		if(!$id)
+		{
+			$result = array('success'=>false);
+		}
+		else
+		{
+			$img = $this->getImageTable()->getImage($id);
+			if($img)
+			{
+				$result = array_merge($img->toArray(),array('success'=>true));
+			}
+			else
+			{
+				$result = array('success'=>false);
+			}
+		}
+        return new JsonModel($result);
 	}
     public function getAction()
     {
@@ -30,5 +47,24 @@ class IndexController extends AbstractActionController
             'id'=>$this->params()->fromRoute('id',false),
         ));
         return $result;
+	}
+	public function getImageTable()
+    {
+        if (!$this->imageTable) {
+            $sm = $this->getServiceLocator();
+            $this->imageTable = $sm->get('Ciscore\Model\ImageTable');
+        }
+        return $this->imageTable;
+    }
+	
+	protected function getImageFromRoute()
+	{
+		//Get ID From Route
+		$id = $this->params()->fromRoute('id',0);
+		//Return False if not set or empty
+		if(!$id)
+			return false;
+		
+		return $id;
 	}
 }
