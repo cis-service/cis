4b2eb6c16b3427c0f8d00a358588e6389bfb7313
Voxel01 <lukas@wmm.at>
2013-09-29 21:16:51 +0200

    Update Applikcationconfig

--- config/application.config.php
+++ config/application.config.php
@@ -2,6 +2,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'Ciscore',
         'Cisapi',
         'Cisweb',
     ),
@@ -46,7 +47,7 @@ return array(
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
-        // 'check_dependencies' => true,
+        'check_dependencies' => true,
     ),
 
     // Used to create an own service manager. May contain one or more child arrays.
